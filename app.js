3d2da96099d55638568e73ef4331ec9c73fbbce9
Om aher <aherom099@gmail.com>
2024-06-13 17:26:47 +0530

    first commitbodyParser

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req,res,next)=>
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product',(req,res,next)=>
     {
-        console.log("in side middle ware");
+        res.send(`<html>
+                  <head>
+                      <title> add porduct</title>
+                  </head>
+                  <body>
+                       <form action="/product" method="POST">
+                        name<input type="text" name="tittle"></input>
+                        <br>size<input type="number" name="size"></input>
+                        <button type="submit">submit</button>
+                       </form>
+                  </body>
+                 </html>`);
+    });
+
+
+    app.use('/product', (req, res, next) => {
+        console.log(req.body); 
+       res.redirect('/');
+    });
+app.use('/',(req,res,next)=>
+    {
+       
         res.send('<h1>Hello from Express</h1>');
     })
-app.listen(4001);
+app.listen(4001); 
