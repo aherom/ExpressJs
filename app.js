d7157df39329a7e3b0a2685cc51beb155aca98cd
Om aher <aherom099@gmail.com>
2024-06-14 10:48:50 +0530

    Add modifications to app.js and package-lock.json files

--- app.js
+++ app.js
@@ -1,33 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const adminrouter = require('./routes/admin');
+const clientrouter = require('./routes/client');
 const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use('/add-product',(req,res,next)=>
-    {
-        res.send(`<html>
-                  <head>
-                      <title> add porduct</title>
-                  </head>
-                  <body>
-                       <form action="/product" method="POST">
-                        name<input type="text" name="tittle"></input>
-                        <br>size<input type="number" name="size"></input>
-                        <button type="submit">submit</button>
-                       </form>
-                  </body>
-                 </html>`);
-    });
-
-
-    app.use('/product', (req, res, next) => {
-        console.log(req.body); 
-       res.redirect('/');
-    });
-app.use('/',(req,res,next)=>
-    {
-       
-        res.send('<h1>Hello from Express</h1>');
-    })
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(adminrouter);
+app.use(clientrouter);
 app.listen(4001); 
