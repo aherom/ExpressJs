52278633b88ab9b790733cc85e386c562c2807e5
Om aher <aherom099@gmail.com>
2024-06-16 13:30:26 +0530

    Update MVC folder and add new files

--- MVC/app.js
+++ MVC/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const root = require('./util/path')
 const adminrouter = require('./routes/admin');
 const clientrouter = require('./routes/shop');
 const contactus = require('./routes/contact')
 const notFound = require('./controler/404');
 const app = express();
 app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static(path.join(root,'public')))
 app.use('/admin',adminrouter);
 app.use(contactus);
 app.use(clientrouter);
