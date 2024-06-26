37f88e42560ca79620b4912016a00d3ea9f7be63
Om aher <132733957+aherom@users.noreply.github.com>
2024-06-26 11:13:25 +0530

    final

--- MVC/util/database.js
+++ MVC/util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   database: 'node',
-  password: 'ANE412410@om'
+  password: 'pass123'
 });
 
 //console.log('Pool created:', pool);
